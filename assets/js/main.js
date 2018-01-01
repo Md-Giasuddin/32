5f1d55f0daba18b9ca74684a011018e8689b8ac4
Md-Giasuddin <33098059+Md-Giasuddin@users.noreply.github.com>
2018-01-01 15:17:26 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+            
+        var $ = skrollr.init({
+            render: function(data) {
+                
+            }
+        });
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
